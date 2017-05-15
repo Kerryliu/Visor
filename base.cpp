016f2c9cb46d554872b93e476165de093b57399a
Kerry Liu <kerryliu@localhost.localdomain>
2017-05-15 16:22:06 -0400

    Ran clang-format.

--- base.cpp
+++ base.cpp
@@ -1,10 +1,7 @@
 #include <gtkmm.h>
 
-int main(int argc, char *argv[])
-{
-  auto app =
-    Gtk::Application::create(argc, argv,
-      "org.gtkmm.examples.base");
+int main(int argc, char *argv[]) {
+  auto app = Gtk::Application::create(argc, argv, "org.gtkmm.examples.base");
 
   Gtk::Window window;
   window.set_default_size(200, 200);
