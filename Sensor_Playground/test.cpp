f2155272aff644f674faa7e716dcb3b575662608
Kerry Liu <kerryliu123@gmail.com>
2017-05-16 11:42:20 -0400

    Whoops.

--- Sensor_Playground/test.cpp
+++ Sensor_Playground/test.cpp
@@ -15,7 +15,7 @@ int main() {
   }
 
   for (auto & d : devices) {
-    std::cout << "Device Name: " << d.get_name() << std::endl;
+    std::cout << "Device Name: " << d.name << std::endl;
     std::cout << "  Temperature Sensor Count: " << d.temp_sensor_count << std::endl;
     std::cout << "  Fan Sensor Count: " << d.fan_sensor_count << std::endl;
   }
