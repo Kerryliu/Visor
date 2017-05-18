f1618e7e9561213e2ce58568bed1174de8a378b1
Kerry Liu <kerryliu123@gmail.com>
2017-05-17 20:54:30 -0400

    Clang format and remove old file

--- window.cpp
+++ window.cpp
@@ -6,8 +6,7 @@
 
 namespace fs = std::experimental::filesystem;
 
-Window::Window()
-    : m_VBox(Gtk::ORIENTATION_VERTICAL), m_Button_Quit("Quit") {
+Window::Window() : m_VBox(Gtk::ORIENTATION_VERTICAL), m_Button_Quit("Quit") {
   set_title("Wow");
   set_border_width(5);
   set_default_size(800, 600);
@@ -34,7 +33,7 @@ Window::Window()
   m_TreeView.set_model(m_refTreeModel);
 
   // All the items to be reordered with drag-and-drop:
-  //m_TreeView.set_reorderable();
+  // m_TreeView.set_reorderable();
   //
   std::vector<Device> devices;
   std::string path = "/sys/class/hwmon/";
@@ -42,7 +41,7 @@ Window::Window()
   for (auto &p : fs::directory_iterator(path)) {
     devices.push_back(Device(p.path().string()));
   }
-  
+
   Gtk::TreeModel::Row row;
   Gtk::TreeModel::Row child_row;
   Gtk::TreeModel::Row baby_child_row;
@@ -56,29 +55,31 @@ Window::Window()
 
     row = *(m_refTreeModel->append());
     row[m_Columns.m_col_name] = devices[i].name;
-    row[m_Columns.m_col_value]= "";
-    
+    row[m_Columns.m_col_value] = "";
+
     // Temperature
-    if(!temp_readings.empty()) {
+    if (!temp_readings.empty()) {
       child_row = *(m_refTreeModel->append(row.children()));
       child_row[m_Columns.m_col_name] = "Temperature: ";
       child_row[m_Columns.m_col_value] = "";
       for (unsigned int j = 0; j < temp_readings.size(); j++) {
         baby_child_row = *(m_refTreeModel->append(child_row.children()));
         baby_child_row[m_Columns.m_col_name] = temp_readings[j].first + ": ";
-        baby_child_row[m_Columns.m_col_value] = std::to_string(temp_readings[j].second/1000);
+        baby_child_row[m_Columns.m_col_value] =
+            std::to_string(temp_readings[j].second / 1000);
       }
     }
 
     // Fan
-    if(!fan_readings.empty()) {
+    if (!fan_readings.empty()) {
       child_row = *(m_refTreeModel->append(row.children()));
       child_row[m_Columns.m_col_name] = "Fan: ";
       child_row[m_Columns.m_col_value] = "";
       for (unsigned int j = 0; j < fan_readings.size(); j++) {
         baby_child_row = *(m_refTreeModel->append(child_row.children()));
         baby_child_row[m_Columns.m_col_name] = fan_readings[j].first + ": ";
-        baby_child_row[m_Columns.m_col_value] = std::to_string(fan_readings[j].second);
+        baby_child_row[m_Columns.m_col_value] =
+            std::to_string(fan_readings[j].second);
       }
     }
   }
@@ -92,14 +93,15 @@ Window::Window()
       sigc::mem_fun(*this, &Window::on_treeview_row_activated));
 
   show_all_children();
+  m_TreeView.expand_all();
 }
 
 Window::~Window() {}
 
 void Window::on_button_quit() { hide(); }
 
-void Window::on_treeview_row_activated(
-    const Gtk::TreeModel::Path &path, Gtk::TreeViewColumn * /* column */) {
+void Window::on_treeview_row_activated(const Gtk::TreeModel::Path &path,
+                                       Gtk::TreeViewColumn * /* column */) {
   Gtk::TreeModel::iterator iter = m_refTreeModel->get_iter(path);
   if (iter) {
     Gtk::TreeModel::Row row = *iter;
